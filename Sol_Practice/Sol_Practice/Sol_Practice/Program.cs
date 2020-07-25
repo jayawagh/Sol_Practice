b6f182b8a2f532c8e22876815bf81b7b4a576174
Jayashri Wagh <jayawagh@outlook.com>
2020-07-25 13:07:49 +0530

    # Jayashri Wagh:init

---
+++ Sol_Practice/Sol_Practice/Sol_Practice/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Sol_Practice
+{
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Console.WriteLine("Hello World!");
+    //    }
+    //}
+    public class ParentClass
+    {
+        public ParentClass()
+        {
+            Console.WriteLine("Hello from Parent Constructor.");
+        }
+        public ParentClass(string name)
+        {
+            Console.WriteLine("Jayashri FROM Parent Constructor.");
+        }
+        public void print()
+        {
+            Console.WriteLine("I'm a Parent Class.");
+        }
+    }
+    public class ChildClass : ParentClass
+    {
+        public ChildClass() 
+        {
+            Console.WriteLine("Child Constructor.");
+        }
+        public static void Main()
+        {
+            ChildClass child = new ChildClass();
+            child.print();
+        }
+    }
+}
